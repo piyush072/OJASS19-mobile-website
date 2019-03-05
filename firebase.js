cf139c7e1c5b751173af7e7aeb56433bd763e360
Gurmeet <gsinghs1998@gmail.com>
2019-03-05 13:59:11 +0530

    Added event info

---
+++ firebase.js
@@ -0,0 +1,13 @@
+<script src="https://www.gstatic.com/firebasejs/5.8.5/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCkoeOktpUqtQhau5OzClOjt-Pqdoe1wxI",
+    authDomain: "ojsaa19.firebaseapp.com",
+    databaseURL: "https://ojsaa19.firebaseio.com",
+    projectId: "ojsaa19",
+    storageBucket: "ojsaa19.appspot.com",
+    messagingSenderId: "568902739392"
+  };
+  firebase.initializeApp(config);
+</script>
