4119897b774d22a3fc32d08905669c71af14141c
Gurmeet <gsinghs1998@gmail.com>
2019-03-17 16:17:19 +0530

    Firebase Data

--- Events/event_info.php
+++ Events/event_info.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en" class="no-js">
 	<head>
@@ -39,7 +40,7 @@
 	}
 	.text{
 		text-align:center;
-		font-family: 'Kumar One Outline', cursive;"
+		font-family: 'Kumar One Outline', cursive;
 		position:relative;
 		left:120px;
 		padding:0;
@@ -60,19 +61,19 @@
 		      <div class="card" style="background:black;" id="cd" style="position:fixed;overflow:hidden;">
 		        <ul class="nav nav-tabs" role="tablist" >
 		          <li role="presentation" class="active"><a  href="#home" aria-controls="home" role="tab" data-toggle="tab"><i class="fa fa-home"></i>&nbsp; </a></li>
-		          <li role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab"><i class="fa fa-pencil"></i>&nbsp; </a></li>
-		          <li role="presentation"><a href="#messages" aria-controls="messages" role="tab" data-toggle="tab"><i class="fa fa-book"></i>&nbsp; </a></li>
-		          <li role="presentation"><a href="#settings" aria-controls="settings" role="tab" data-toggle="tab"><i class="fa fa-trophy"></i>&nbsp; </a></li>
-		          <li role="presentation"><a href="#extra" aria-controls="settings" role="tab" data-toggle="tab"><i class="fa fa-user"></i>&nbsp;</a></li>
+		          <li role="presentation"><a href="#details" aria-controls="details" role="tab" data-toggle="tab"><i class="fa fa-pencil"></i>&nbsp; </a></li>
+		          <li role="presentation"><a href="#rules" aria-controls="rules" role="tab" data-toggle="tab"><i class="fa fa-book"></i>&nbsp; </a></li>
+		          <li role="presentation"><a href="#prizes" aria-controls="prizes" role="tab" data-toggle="tab"><i class="fa fa-trophy"></i>&nbsp; </a></li>
+		          <li role="presentation"><a href="#co" aria-controls="co" role="tab" data-toggle="tab"><i class="fa fa-user"></i>&nbsp;</a></li>
 		        </ul>
 
 		        <!-- Tab panes -->
 		        <div class="tab-content" style="max-height:300px;overflow-y: auto;">
-		          <div role="tabpanel" class="tab-pane active" id="home"><span class="text">About</span><br>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset </div>
-		          <div role="tabpanel" class="tab-pane" id="profile"><span class="text">Details</span><br>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</div>
-		          <div role="tabpanel" class="tab-pane" id="messages"><span class="text">Rules</span><br>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>
-		          <div role="tabpanel" class="tab-pane" id="settings"><span class="text">Prizes</span><br>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passage..</div>
-		          <div role="tabpanel" class="tab-pane" id="extra"><span class="text" style="left:90px;">Co-ordinators</span><br>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passage..</div>
+		          <div role="tabpanel" class="tab-pane active" id="home"><span class="text">About</span><br><span id="hometext"></span></div>
+		          <div role="tabpanel" class="tab-pane" id="details"><span class="text">Details</span><br><span id="detailtext"></span></div>
+		          <div role="tabpanel" class="tab-pane" id="rules"><span class="text">Rules</span><br><span id="ruletext"></span></div>
+		          <div role="tabpanel" class="tab-pane" id="prizes"><span class="text">Prizes</span><br><span id="prizetext"></span></div>
+		          <div role="tabpanel" class="tab-pane" id="co"><span class="text" style="left:90px;">Co-ordinators</span><br><span id="cotext"></span></div>
 		        </div>
 		      </div>
 		    </div>
@@ -83,41 +84,67 @@
 				<button class="cn-button" id="cn-button" style="position:fixed;top:530px;">Events</button>
 				<div class="cn-wrapper" id="cn-wrapper" style="position:fixed;top:530px;">
 					<ul class="eve">
-						<li><a href="#"><span>About</span></a></li>
-						<li><a href="#"><span>Tutorials</span></a></li>
-						<li><a href="#"><span>Articles</span></a></li>
-						<li><a href="#"><span>Snippets</span></a></li>
-						<li><a href="#"><span>Plugins</span></a></li>
-						<li><a href="#"><span>Contact</span></a></li>
-					 </ul>
+					
+					<script>
+						
+							var root = firebase.database().ref();
+							root.child('/Events/<?php echo $_GET["key"]; ?>').once('value', function(snap){
+									var j=0;var event = snap.key;
+									snap.forEach(function(childSnap){
+											var e_name = childSnap.key;
+											$('.eve').append('<li><a href="#" class="event_name" id="ev'+j+'"><span>'+childSnap.key+'</span></a></li>');
+											var id = "#ev"+j;
+											j++;						
+											$(id).click(function(){
+												$('#prizetext').text(" ");
+												$('#cotext').text(" ");
+												$('#ruletext').text(" ");
+												$('#hometext').text(childSnap.child("About").val());
+												$('#detailtext').text(childSnap.child("Details").val());
+												$('#prizetext').append("First: "+childSnap.child("Prizes").child("First").val()+"<br>Second: "+childSnap.child("Prizes").child("Second").val()+"<br>Third: "+childSnap.child("Prizes").child("Third").val()+"<br>Total: "+childSnap.child("Prizes").child("Total").val());
+												root.child('/Events/'+event+'/'+e_name+'/Co-ordinators').once('value', function(snap1){
+													snap1.forEach(function(childSnap1){
+													$('#cotext').append(childSnap1.key+" : "+childSnap1.val()+"<br>");});
+												});
+												root.child('/Events/'+event+'/'+e_name+'/Rules').once('value', function(snap1){
+													snap1.forEach(function(childSnap1){
+													$('#ruletext').append(childSnap1.key+" : "+childSnap1.val()+"<br>");});
+												});
+											});
+									});
+									var i=1;var a = 30;var b= -30;
+									for(i=1;i<=j;i++){
+										$('.eve li:nth-child('+i+')').css({
+											'-webkit-transform' : 'rotate('+b+'deg) skew(60deg)',
+											'-moz-transform'    : 'rotate('+b+'deg) skew(60deg)',
+											'-ms-transform'     : 'rotate('+b+'deg) skew(60deg)',
+											'transform'         : 'rotate('+b+'deg) skew(60deg)'
+										});
+										b+=a;
+									}
+							});
+							
+				</script>
+				
+					</ul>
 				</div>
 				<!-- End of Nav Structure -->
 			</div>
+
+		
+
+
 			<script type="text/javascript">
 				$(document).ready(function(){
-					$('a').click(function(){
+					$('.eve').click(function(){
 						$('#row').css("display","block");
 						$('#image').css("display","none");
 					});
-					
-					var i=1;var a = 180/6;var b=-20;
-					for(i=1;i<=6;i++){
-						$('.eve li:nth-child('+i+')').css({
-							'-webkit-transform' : 'rotate('+b+'deg) skew(60deg)',
-							'-moz-transform'    : 'rotate('+b+'deg) skew(60deg)',
-							'-ms-transform'     : 'rotate('+b+'deg) skew(60deg)',
-							'transform'         : 'rotate('+b+'deg) skew(60deg)'
-						});
-						b+=a;
-					}
 				});
 			</script>
 		</div><!-- /container -->
 		<script src="js/polyfills.js"></script>
 		<script src="js/demo2.js"></script>
-		<script>
-
-
-		</script>
+		
 	</body>
 </html>
